ea9c8d46d778842ed6117bdd1bdda9087ee42ec7
trankimbao1472000 <73160170+trankimbao1472000@users.noreply.github.com>
2021-09-28 22:35:41 +0700

    form Promotion and receipts

---
+++ DACK-13-BuiXuanHieu/BUS/BusPromotions.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DACK_13_BuiXuanHieu.DAO;
+using System.Windows.Forms;
+using System.Data.Entity.Infrastructure;
+
+namespace DACK_13_BuiXuanHieu.BUS
+{
+    class BusPromotions
+    {
+        DaoPromotions daoPromotions;
+
+        //
+        public BusPromotions()
+        {
+            //
+            daoPromotions = new DaoPromotions();
+        }
+        public void displayTablePromotions(DataGridView dgvOrders)
+        {
+            //
+            dgvOrders.DataSource = daoPromotions.loadTablePromotions();
+
+            //
+            dgvOrders.Columns[0].Width = (int)(0.25 * dgvOrders.Width);
+            dgvOrders.Columns[1].Width = (int)(0.3 * dgvOrders.Width);
+            //dgvOrders.Columns[2].Width = (int)(0.31 * dgvOrders.Width);
+            //dgvOrders.Columns[3].Width = (int)(0.2 * dgvOrders.Width);
+        }
+        public bool addRecord(TextBox tbPromotionName, TextBox tbDescription)
+        {
+            //
+            String promotionName = tbPromotionName.Text.Trim(),
+                   description = tbDescription.Text.Trim();
+
+
+            //
+            if (promotionName == "" || description == "")
+            {
+                MessageBox.Show("Please, fill up ALL attributes !");
+                return false;
+            }
+
+            else
+            {
+                DialogResult dr = MessageBox.Show("A record will be ADDED! Continue ?", "Action confirm",
+                                                  MessageBoxButtons.OKCancel,
+                                                  MessageBoxIcon.Question);
+                if (dr == DialogResult.Cancel)
+                {
+                    return false;
+                }
+                else
+                {
+                    try
+                    {
+                        Promotion s = new Promotion();
+                        s.Description = description;
+                        s.PromotionName = promotionName;
+
+
+
+
+                        if (daoPromotions.addRecord(s))
+                        {
+                            MessageBox.Show("Successfully !", "Announcement",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Information);
+                        }
+                        else
+                        {
+                            MessageBox.Show("Fail ! Something crashed in DataAccessLayer ?!!", "Announcement",
+                                            MessageBoxButtons.OK,
+                                            MessageBoxIcon.Error);
+                            return false;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        MessageBox.Show(e.Message.ToString());
+                        return false;
+                    }
+                }
+
+                return true;
+            }
+        }
+        public bool editRecord(DataGridView dgvPromotions, TextBox tbPromotionName, TextBox tbDescription)
+        {
+            //
+            String promotionName = tbPromotionName.Text.Trim(),
+                   description = tbDescription.Text.Trim();
+
+            //
+            String promotionID = dgvPromotions.CurrentRow.Cells["PromotionID"].Value.ToString();
+
+
+            //
+            DialogResult dr = MessageBox.Show("Record [ " + promotionID + " ] will be EDITED! Continue ?", "Action confirm",
+                                            MessageBoxButtons.OKCancel,
+                                            MessageBoxIcon.Question);
+            if (dr == DialogResult.OK)
+            {
+                try
+                {
+
+                    Promotion s = new Promotion();
+                    s.Description = description;
+                    s.PromotionName = promotionName;
+                    s.PromotionID = int.Parse(promotionID);
+
+
+                    if (daoPromotions.editRecord(s))
+                    {
+                        MessageBox.Show("Successfully !", "Announcement",
+                                    MessageBoxButtons.OK,
+                                    MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Fail ! Something crashed in DataAccessLayer ?!!", "Announcement",
+                                        MessageBoxButtons.OK,
+                                        MessageBoxIcon.Error);
+                        return false;
+                    }
+                }
+                catch (Exception e)
+                {
+                    MessageBox.Show(e.Message.ToString());
+                    return false;
+                }
+            }
+            return true;
+        }
+        
+
+        public void removeRecord(Promotion p)
+        {
+
+            if (daoPromotions.Check(p))
+            {
+                try
+                {
+                    daoPromotions.removeRecord(p);
+                    MessageBox.Show("Remove success");
+                }
+                catch(DbUpdateException ex)
+                {
+                    MessageBox.Show("Remove fail\n" + ex.Message);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Remove fail\n" );
+            }
+            //
+            //    //String promotionID = dgvPromotions.CurrentRow.Cells["PromotionID"].Value.ToString();
+
+
+            //    //
+            //    DialogResult dr = MessageBox.Show("Record [ " + promotionID + " ] will be REMOVED! Continue ?", "Action confrim",
+            //                            MessageBoxButtons.OKCancel,
+            //                            MessageBoxIcon.Question);
+            //    if (dr == DialogResult.OK)
+            //    {
+            //        try
+            //        {
+            //            if (daoPromotions.removeRecord(Promotion p)))
+            //            {
+            //                MessageBox.Show("Successfully !", "Announcement",
+            //                                MessageBoxButtons.OK,
+            //                                MessageBoxIcon.Information);
+            //            }
+            //            else
+            //            {
+            //                MessageBox.Show("Fail ! Something crashed in DataAccessLayer ?!!", "Announcement",
+            //                                MessageBoxButtons.OK,
+            //                                MessageBoxIcon.Error);
+            //                return false;
+            //            }
+            //        }
+            //        catch (Exception e)
+            //        {
+            //            MessageBox.Show(e.Message.ToString());
+            //            return false;
+            //        }
+            //    }
+            //    return true;
+        }
+    }
+}
+
---
+++ DACK-13-BuiXuanHieu/DAO/DaoReceipts.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DACK_13_BuiXuanHieu.DAO
+{
+    class DaoReceipts
+    {
+        SupermarketEntities supmar;
+        public DaoReceipts()
+        {
+            supmar = new SupermarketEntities();
+        }
+        public dynamic loadTableReceipts()
+        {
+            //
+            dynamic reciepts = supmar.Receipts.Select(s => new
+            {
+                s.ReceiptID,
+                s.EmployeeID,
+                s.CustomerID,
+                s.ReceiveDate,
+                s.ReceiveMethod
+            }).ToList();
+
+            return reciepts;
+        }
+        public bool addRecord(Receipt s)
+        {
+            //
+            try
+            {
+                supmar.Receipts.Add(s);
+                supmar.SaveChanges();
+                return true;
+            }
+            catch //(Exception e)
+            {
+                //MessageBox.Show(e.Message.ToString());
+                return false;
+            }
+        }
+        public bool editRecord(Receipt newRecord)
+        {
+            //
+            try
+            {
+                Receipt oldRecord = supmar.Receipts.First(s => s.ReceiptID == newRecord.ReceiptID);
+                oldRecord.EmployeeID = newRecord.EmployeeID;
+                oldRecord.CustomerID = newRecord.CustomerID;
+                oldRecord.ReceiveDate = newRecord.ReceiveDate;
+                oldRecord.ReceiveMethod = newRecord.ReceiveMethod;
+
+                supmar.SaveChanges();
+                return true;
+            }
+            catch //(Exception e)
+            {
+                //MessageBox.Show(e.Message.ToString());
+                return false;
+            }
+        }
+        public bool removeRecord(int ReceiptID)
+        {
+            //
+            try
+            {
+                Receipt chosenRecord = supmar.Receipts.First(s => s.ReceiptID == ReceiptID);
+                supmar.Receipts.Remove(chosenRecord);
+                supmar.SaveChanges();
+                return true;
+            }
+            catch //(Exception e)
+            {
+                //MessageBox.Show(e.Message.ToString());
+                return false;
+            }
+        }
+
+    }
+}
